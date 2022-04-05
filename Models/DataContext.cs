9e55c02eccc6705b57647132f60627bc81053022
Aleksandr <alexkolpak2003@gmail.com>
2022-04-05 17:30:48 +0500

    Add project files.

---
+++ Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace family_budget.Models
+{
+    class DataContext : DbContext
+    {
+
+    }
+}
