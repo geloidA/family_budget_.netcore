14d8e2213c5a37d95e798520da897707c04cc3f2
Aleksandr <alexkolpak2003@gmail.com>
2022-04-06 08:55:37 +0500

    models and first customization db

---
+++ Models/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace family_budget.Models
+{
+    class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<FamilyMember> FamilyMembers { get; set; }
+
+        public DataContext() : base()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
