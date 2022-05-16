9fc3d273099bae6f5afc76e40c59f199acec34e0
Aleksandr <alexkolpak2003@gmail.com>
2022-05-16 09:18:03 +0500

    Change expenses

---
+++ ViewModels/ChangingExpensesWndViewModel.cs
@@ -0,0 +1,33 @@
+﻿using DevExpress.Mvvm;
+using family_budget.Models;
+using family_budget.Models.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace family_budget.ViewModels
+{
+    internal class ChangingExpensesWndViewModel : AddingTransactionModel
+    {
+        private Expense changeExpense;
+
+        public ChangingExpensesWndViewModel(Expense changeExpense) : base()
+        {
+            this.changeExpense = changeExpense;
+            Cost = changeExpense.Cost;
+            Date = changeExpense.Date;
+            Description = changeExpense.Description;
+            SelectedFamilyMember = DataWorker.FamilyMembers.FirstOrDefault(m => m.Id == changeExpense.FamilyMemberId);
+            Classification = changeExpense.Classification;
+        }
+
+        public override ICommand AddTransaction => 
+            new DelegateCommand(() =>
+            {
+                //TODO 
+            }); 
+    }
+}
